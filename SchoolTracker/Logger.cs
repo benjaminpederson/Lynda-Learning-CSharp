2d0a4d07f4240b4b1e078988a13785e2c342f2f4
Benjamin Pederson <pedersoncoding@gmail.com>
2019-07-31 20:48:19 -0700

    Update Logger.cs

--- SchoolTracker/Logger.cs
+++ SchoolTracker/Logger.cs
@@ -9,7 +9,7 @@ namespace SchoolTracker
         const string DefualtSystemName = "SchoolTracker";
         public static void Log(string msg, string system = DefualtSystemName, int priority = 1)
         {
-            Console.WriteLine("System: {0}, Priority: {1}, Msg: {2}", system, priority, msg);
+            Console.WriteLine($"System: {system}, Priority: {priority}, Msg: {msg}");
         }
     }
 }
