78ebd89251e7c89053f8ced7c961e90507d6f128
Benjamin Pederson <pedersoncoding@gmail.com>
2019-07-31 17:01:07 -0700

    Uploaded Project Main Folders

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, thank you for taking the time to participate in our survey.  Please answer the following:");
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Thanks, you entered:");
+            Console.WriteLine("Your name is {0}.", name);
+            Console.WriteLine("You are {0} years old.", age);
+            Console.WriteLine("You were born in {0}.", month);
+        }
+    }
+}
---
+++ Loop Counting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repeat = 0;
+
+            while (repeat < 5)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+                for (int i = 10; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+
+                repeat++;
+            }
+            Console.WriteLine("Iterations, {0}", repeat);
+
+        }
+    }
+}
