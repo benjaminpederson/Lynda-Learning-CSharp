92c7580fe2b45130a4fd7bc591ba2752cbc7ad48
Benjamin Pederson <pedersoncoding@gmail.com>
2019-07-31 18:03:28 -0700

    Update Program.cs

--- SchoolTracker/Program.cs
+++ SchoolTracker/Program.cs
@@ -1,29 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace SchoolTracker
 {
+    enum School{
+        Harvard,
+        MIT,
+        Stanford
+    }
     class Program
     {
+        static List<Student> students = new List<Student>();
         static void Main(string[] args)
         {
-            var students = new List<Student>();
-
             var adding = true;
             
             while (adding)
             {
+                try
+                {
                 var newStudent = new Student();
 
                 newStudent.Name = Util.Console.Ask("Student Name:");
 
-                newStudent.Grade = int.Parse(Util.Console.Ask("Student Grade:"));
+                newStudent.Grade = Util.Console.AskInt("Student Grade: ");
+
+                newStudent.School = (School) Util.Console.AskInt("School Name (Corresponding #): \n 0: Harvard \n 1: MIT \n 2: Standford \n");
 
                 newStudent.Birthday = Util.Console.Ask("Student Birthday:");
 
                 newStudent.Address = Util.Console.Ask("Student Address:");
 
-                newStudent.Phone = int.Parse(Util.Console.Ask("Student Phone:"));
+                newStudent.Phone = Util.Console.AskInt("Student Phone:");
 
                 students.Add(newStudent);
                 Student.Count++;
@@ -33,17 +41,49 @@ namespace SchoolTracker
 
                 if (Console.ReadLine() != "y")
                     adding = false;
+                }
+
+                catch (FormatException msg)
+                {
+                    Console.WriteLine(msg.Message);
+                }
+                
+                catch (Exception)
+                {
+                    Console.WriteLine("Error adding student,  please try again.");
+                }
             }
 
             foreach (var student in students)
             {
                 Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
-            }            
+            }
+
+            Exports();            
         }
 
         static void Import()
         {
             var importedStudent = new Student();
+        }
+
+        static void Exports()
+        {
+            foreach (var student in students)
+            {
+                switch(student.School)
+                {
+                    case School.Harvard:
+                        Console.WriteLine("Exporting to Harvard.");
+                        break;
+                    case School.MIT:
+                        Console.WriteLine("Exporting to MIT.");
+                        break;
+                    case School.Stanford:
+                        Console.WriteLine("Exporting to Stanford.");
+                        break;
+                }
+            }
         }
     }
 
@@ -64,6 +104,7 @@ namespace SchoolTracker
         static public int Count = 0;
         public int Grade;
         public string Birthday;
+        public School School;
     
 
         public Student()
