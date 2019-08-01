92fdc8ca696ca4cef12ae01fcfdf88d2cb7ecca9
Benjamin Pederson <pedersoncoding@gmail.com>
2019-07-31 17:03:02 -0700

    Added Project Folders

---
+++ Passcode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pass="";
+
+            while (pass != "secret")
+            {
+                Console.WriteLine("Please enter the secure passcode to confirm your access.");
+                pass = Console.ReadLine();
+
+                if (pass != "secret")
+                {
+                    Console.WriteLine("Invalid Passcode, Authorization Denied");
+                }
+            }
+            Console.WriteLine("User Authentication Complete, Access Granted");
+        }
+    }
+}
---
+++ Survey/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        class Data
+    {
+        public string Name;
+        public int Age;
+        public string Month;
+
+        public void Display()
+        {
+            Console.WriteLine("Thanks, you entered:");
+            Console.WriteLine("Your name is {0}.", Name);
+            Console.WriteLine("You are {0} years old.", Age);
+            Console.WriteLine("You were born in {0}.", Month);
+
+            switch (Month)
+            {
+                case "march":
+                case "March":
+                    Console.WriteLine("You are an Aries.");
+                    break;
+                case "april":
+                case "April":
+                    Console.WriteLine("You are a Taurus.");
+                    break;
+                case "may":
+                case "May":
+                    Console.WriteLine("You are a Gemini.");
+                    break;
+            }
+        }
+    }
+        static void Main(string[] args)
+        {
+            var data = new Data();
+
+            Console.WriteLine("Hello, thank you for taking the time to participate in our survey.  Please answer the following:");
+
+            Console.WriteLine("What is your name?");
+            data.Name = QuestionAnswer();
+
+            Console.WriteLine("How old are you?");
+            data.Age = int.Parse(QuestionAnswer());
+
+            Console.WriteLine("What month were you born in?");
+            data.Month = QuestionAnswer();
+
+            data.Display();
+
+        }
+         
+        static string QuestionAnswer()
+        {
+            var answer = Console.ReadLine();
+            if (answer == "")
+            {
+                Console.WriteLine("You did not type anything, please try again.");
+                return Console.ReadLine();
+            }
+            return answer;
+        }
+    }
+}
